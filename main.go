df6748d259f4515f0937990eb953c3e917957a5c
Almaz Iskhakov <almaz.iskhakov@flatstack.com>
2017-10-25 02:56:54 +0300

    q

--- main.go
+++ main.go
@@ -1,29 +1,96 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+"encoding/json"
+"fmt"
+"log"
+"net/http"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+"github.com/gorilla/mux"
+"github.com/gorilla/schema"
 )
 
-func main() {
-	port := os.Getenv("PORT")
+// Movie Struct
+type Movie struct {
+	Title  string `json:"title"`
+	Rating string `json:"rating"`
+	Year   string `json:"year"`
+}
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+var movies = map[string]*Movie{
+	"tt0076759": &Movie{Title: "Star Wars: A New Hope", Rating: "8.7", Year: "1977"},
+	"tt0082971": &Movie{Title: "Indiana Jones: Raiders of the Lost Ark", Rating: "8.6", Year: "1981"},
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", hello).Methods("POST")
+	router.HandleFunc("/movie/{imdbKey}", handleMovie).Methods("GET", "DELETE", "POST")
+	http.ListenAndServe(":8080", router)
+}
+
+func handleMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(req)
+	imdbKey := vars["imdbKey"]
+
+	switch req.Method {
+	case "GET":
+		movie, ok := movies[imdbKey]
+		if !ok {
+			res.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(res, string("Movie not found"))
+		}
+		outgoingJSON, error := json.Marshal(movie)
+		if error != nil {
+			log.Println(error.Error())
+			http.Error(res, error.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprint(res, string(outgoingJSON))
+	case "DELETE":
+		delete(movies, imdbKey)
+		res.WriteHeader(http.StatusNoContent)
+	case "POST":
+		movie := new(Movie)
+		decoder := json.NewDecoder(req.Body)
+		error := decoder.Decode(&movie)
+		if error != nil {
+			log.Println(error.Error())
+			http.Error(res, error.Error(), http.StatusInternalServerError)
+			return
+		}
+		movies[imdbKey] = movie
+		outgoingJSON, err := json.Marshal(movie)
+		if err != nil {
+			log.Println(error.Error())
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+		fmt.Fprint(res, string(outgoingJSON))
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	log.Println("hello here")
+	res.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+
+	err := req.ParseForm()
+	if err != nil {
+		// handle error
+	}
+	person := new(Person) // Person being a struct type
+	decoder := schema.NewDecoder()
+
+	err = decoder.Decode(person, req.Form)
+	if err != nil {
+		// handle error
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	log.Println(person.Text)
+}
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+type Person struct {
+	Text string `json:"text"`
 }
