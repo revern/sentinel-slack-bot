f38396715bff11e7ea427458eeeaca5b24f380c1
Almaz Iskhakov <almaz.iskhakov@flatstack.com>
2017-10-25 12:05:39 +0300

    simple code changed

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/hello/{name}", index).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
