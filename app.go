38557b34915d8ce9675f122fc5c71723401d69d2
Almaz Iskhakov <almaz.iskhakov@flatstack.com>
2017-10-27 02:13:46 +0300

    add adding

--- app.go
+++ app.go
@@ -29,15 +29,15 @@ func (a *App) Initialize(dbUrl string) {
 	}
 
 	_, err = a.DB.Exec("CREATE TABLE IF NOT EXISTS " +
-			`device("name" PRIMARY KEY,` +
-			`"location" varchar(50) DEFAULT box)`)
+		`device("name" PRIMARY KEY,` +
+		`"location" varchar(50) DEFAULT box)`)
 
 	a.Router = mux.NewRouter()
 	a.initializeRoutes()
 }
 
 func (a *App) Run(port string) {
-	log.Fatal(http.ListenAndServe(":" + port, a.Router))
+	log.Fatal(http.ListenAndServe(":"+port, a.Router))
 }
 
 func (a *App) initializeRoutes() {
@@ -58,41 +58,7 @@ func (a *App) getDevices(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, devices)
 }
 
-
 func (a *App) takeDevice(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-
-	if err != nil {
-		fmt.Println("Error parsing form")
-	}
-
-	msg := new(slack_message)
-	decoder := schema.NewDecoder()
-
-	err = decoder.Decode(msg, r.Form)
-
-	if err != nil {
-		fmt.Println("Error decoding")
-	}
-
-	//d := device{Name: msg.Text}
-	//if err := d.updateDevice(a.DB); err != nil {
-	//	switch err {
-	//	case sql.ErrNoRows:
-	//		respondWithError(w, http.StatusNotFound, "Device not found")
-	//	default:
-	//		respondWithError(w, http.StatusInternalServerError, err.Error())
-	//	}
-	//	return
-	//}
-
-	fmt.Fprint(w, msg.UserName + " take " + msg.Text)
-
-	//respondWithJSON(w, http.StatusOK, d)
-}
-
-//TODO rework
-func (a *App) returnDevice(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 
 	if err != nil {
@@ -119,26 +85,68 @@ func (a *App) returnDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, msg.UserName + " take " + msg.Text)
+	fmt.Fprint(w, msg.UserName+" take "+msg.Text)
+
+	//respondWithJSON(w, http.StatusOK, d)
+}
+
+//TODO rework
+func (a *App) returnDevice(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("Error parsing form")
+	}
+
+	msg := new(slack_message)
+	decoder := schema.NewDecoder()
+
+	err = decoder.Decode(msg, r.Form)
+
+	if err != nil {
+		fmt.Println("Error decoding")
+	}
+
+	d := device{Name: msg.Text}
+	if err := d.updateDevice(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Device not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	fmt.Fprint(w, msg.UserName+" take "+msg.Text)
 
 	//respondWithJSON(w, http.StatusOK, d)
 }
 
 func (a *App) addDevice(w http.ResponseWriter, r *http.Request) {
-	var d device
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&d); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("Error parsing form")
 	}
-	defer r.Body.Close()
+
+	msg := new(slack_message)
+	decoder := schema.NewDecoder()
+
+	err = decoder.Decode(msg, r.Form)
+
+	if err != nil {
+		fmt.Println("Error decoding")
+	}
+
+	d := device{Name: msg.Text}
 
 	if err := d.createDevice(a.DB); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, d)
+	fmt.Fprint(w, "New device < "+msg.Text+" > was added to collection")
+
+	//respondWithJSON(w, http.StatusCreated, d)
 }
 
 func (a *App) deleteDevice(w http.ResponseWriter, r *http.Request) {
@@ -169,7 +177,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	w.Write(response)
 }
-
 
 func (a *App) handlePing(res http.ResponseWriter, req *http.Request) {
 	time.Sleep(2 * time.Minute)
